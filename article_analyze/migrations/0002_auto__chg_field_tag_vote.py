2c713f693359adee6297afa568ea13da863f8caa
Rachit Nanda <rnanda1243@gmail.com>
2014-02-04 20:55:20 -0800

    south migrations

---
+++ article_analyze/migrations/0002_auto__chg_field_tag_vote.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Tag.vote'
+        db.alter_column(u'article_analyze_tag', 'vote', self.gf('django.db.models.fields.BooleanField')())
+
+    def backwards(self, orm):
+
+        # Changing field 'Tag.vote'
+        db.alter_column(u'article_analyze_tag', 'vote', self.gf('django.db.models.fields.NullBooleanField')(null=True))
+
+    models = {
+        u'article_analyze.article': {
+            'Meta': {'object_name': 'Article'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200'})
+        },
+        u'article_analyze.tag': {
+            'Meta': {'object_name': 'Tag'},
+            'article': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['article_analyze.Article']"}),
+            'confidence': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '5', 'decimal_places': '4'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'service': ('django.db.models.fields.IntegerField', [], {}),
+            'tag': ('django.db.models.fields.TextField', [], {}),
+            'vote': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        }
+    }
+
+    complete_apps = ['article_analyze']
